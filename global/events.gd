b6f55da2a325a47e2015e13cc56c98ab82ec2b6c
Leo Burén <leo.buren@hotmail.com>
2025-08-11 04:41:36 +0200

    cleanup

--- global/events.gd
+++ global/events.gd
@@ -1,8 +1,6 @@
 extends Node
 
 # Card-related events
-signal card_drag_started(card_ui: CardUI)
-signal card_drag_finished(card_ui: CardUI)
 signal card_aim_started(card_ui: CardUI)
 signal card_aim_finished(card_ui: CardUI)
 signal card_played(card: Card)
